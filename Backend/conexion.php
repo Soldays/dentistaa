76891134e29a26c3c92dcf4d23b7641ae7b51961
Soldays <90343783+Soldays@users.noreply.github.com>
2025-05-15 00:53:11 -0600

    Update conexion.php

--- Backend/conexion.php
+++ Backend/conexion.php
@@ -1,13 +1,14 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
+$servername = "23.21.179.90";
+$username = "username";
+$password = "password";
 $bdname = "users";
-
+ 
 $conn = new mysqli($servername, $username, $password, $bdname);
-
+ 
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
-
+    
+ 
 ?>
